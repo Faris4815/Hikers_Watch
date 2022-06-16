826140777f716ea43e2b169902b168fc50f71fe0
Faris Beginovic <far.beg@web.de>
2022-06-16 13:06:39 +0200

    This was the first try to implement the Permission-Requests and also using the shouldShowRequestPermissionRationale() Methode. Its not working out as I intended tho so I want to try an other implementation. Keeping this for reference.

--- app/src/main/java/com/example/hikerswatch/MainActivity.java
+++ app/src/main/java/com/example/hikerswatch/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.hikerswatch;
 
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+
+import java.security.Permission;
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +26,7 @@ public class MainActivity extends Activity {
     TextView accuracy;
     TextView altitude;
     TextView address;
+    ArrayList<String> permissionsToBeRequested = new ArrayList<>();
 
 
     @Override
@@ -30,20 +40,83 @@ public class MainActivity extends Activity {
         altitude = findViewById(R.id.altitude_ID);
         address = findViewById(R.id.address_ID);
 
-        //Es gibt viel bessere Wege die Permissions anzufragen aber ich folge hier mal den sehr simplen Weg der im Tutorial gezeigt wurde um voran zu kommen.
-        //Im Internet werden oft ActivityResultLauncher benutzt. Siehe Artikel in Chrome Lesezeichen -> Android -> CheckOut
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            // You can use the API that requires the permission.
-            //performAction(...);
-        } else {
-            // You can directly ask for the permission.
-            // The registered ActivityResultCallback gets the result of this request.
-            //requestPermissionLauncher.launch(
-            //Manifest.permission.ACCESS_COARSE_LOCATION);
+        requestPermissions();
+    }
+
+
+    public boolean hasPermission_COARSE_LOCATION(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION ) == PackageManager.PERMISSION_GRANTED){
+            return true;
+        } else return false;
+    };
+
+    public boolean hasPermission_FINE_LOCATION(){
+        if(ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED){
+            return true;
+        } else return false;
+    };
+
+
+    public void requestPermissions(){
+        ArrayList<String> permissionsToBeRequested = new ArrayList<>();
+
+        if (!hasPermission_COARSE_LOCATION()) {
+            permissionsToBeRequested.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+        if(!hasPermission_FINE_LOCATION()) {
+            permissionsToBeRequested.add(ACCESS_FINE_LOCATION);
+        }
+
+        //checking if PermissionRationale should be shown and displaying dialogue, otherwise directly request Permission.
+        if(!permissionsToBeRequested.isEmpty()) {
+            for (int permissionIndex=0; permissionIndex <= permissionsToBeRequested.size()-1; permissionIndex++){
+                if (shouldShowRequestPermissionRationale(permissionsToBeRequested.get(permissionIndex))){
+
+                    String currentPermission = permissionsToBeRequested.get(permissionIndex);
+
+                    switch (currentPermission) {
+                        case Manifest.permission.ACCESS_COARSE_LOCATION:
+                            showDialogue("Permission needed", "The app needs the approximate Location of you to run properly", permissionIndex);
+                            break;
+
+                        case Manifest.permission.ACCESS_FINE_LOCATION:
+                            showDialogue("Permission needed", "The app needs the fine Location of you to run properly", permissionIndex);
+                            break;
+                    }
+                } else {
+                    ActivityCompat.requestPermissions(this, permissionsToBeRequested.toArray(new String[0]), 0);
+                }
+
+            }
 
         }
 
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+
+    }
+
+    //The permission in the dialogue is the permission  we want to show the dialogue for
+    public void showDialogue(String title, String message, int permissionIndex){
+        new AlertDialog.Builder(this)
+                .setTitle("Permission needed")
+                .setMessage("The App needs you approximate Location to work properly")
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[] {permissionsToBeRequested.get(permissionIndex)}, 0);
+                    }
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                })
+                .create().show();
     }
 }
