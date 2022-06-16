aa02519c8cd2551b06684842af83ee9d63ca1cdb
Faris Beginovic <far.beg@web.de>
2022-06-16 15:06:03 +0200

    Working fine for requesting one permission. Now i want to request multiple Permissions

--- app/src/main/java/com/example/hikerswatch/MainActivity.java
+++ app/src/main/java/com/example/hikerswatch/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.hikerswatch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -30,20 +33,46 @@ public class MainActivity extends Activity {
         altitude = findViewById(R.id.altitude_ID);
         address = findViewById(R.id.address_ID);
 
-        //Es gibt viel bessere Wege die Permissions anzufragen aber ich folge hier mal den sehr simplen Weg der im Tutorial gezeigt wurde um voran zu kommen.
-        //Im Internet werden oft ActivityResultLauncher benutzt. Siehe Artikel in Chrome Lesezeichen -> Android -> CheckOut
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            // You can use the API that requires the permission.
-            //performAction(...);
-        } else {
-            // You can directly ask for the permission.
-            // The registered ActivityResultCallback gets the result of this request.
-            //requestPermissionLauncher.launch(
-            //Manifest.permission.ACCESS_COARSE_LOCATION);
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Log.i("MyInfo","mExec1");
+            // You can use the API that requires the permission.
+            //TODO: performAction(...);
+        } else if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            Log.i("MyInfo","mExec2");
+            // In an educational UI, explain to the user why your app requires this
+            // permission for a specific feature to behave as expected. In this UI,
+            // include a "cancel" or "no thanks" button that allows the user to
+            // continue using your app without granting the permission.
+            //TODO: showInContextUI(...);
+            showDialogue("COARSE-LOCATION is needed", "The App needs your approximate location to work", Manifest.permission.ACCESS_COARSE_LOCATION);
+        } else {
+            Log.i("MyInfo","mExec3");
+            // You can directly ask for the permission.
+            requestPermissions(new String[] { Manifest.permission.ACCESS_COARSE_LOCATION }, 0);
         }
 
 
+    }
+
+
+
+    public void showDialogue(String title, String message, String permission){
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[] {permission}, 0);
+                    }
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                })
+                .create().show();
     }
 }
