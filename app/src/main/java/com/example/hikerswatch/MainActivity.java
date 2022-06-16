b39ead21c63dd9d5a035f9f8d34a31bc7d5a93b6
Faris Beginovic <far.beg@web.de>
2022-06-16 17:50:06 +0200

    Die Funktionalität der App ist da ABER ich muss noch das korrekte Abfragen der Permissions implementieren. Wenn man die Permissions jedoch hat, funktioniert die App. Ich möchte auch die shouldShowRequestPermissionRationale() Methode benutzen.

--- app/src/main/java/com/example/hikerswatch/MainActivity.java
+++ app/src/main/java/com/example/hikerswatch/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.hikerswatch;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
@@ -9,9 +9,21 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Debug;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends Activity {
 
@@ -20,6 +32,11 @@ public class MainActivity extends Activity {
     TextView accuracy;
     TextView altitude;
     TextView address;
+    Button button;
+    List<String> permissionsToRequest = new ArrayList<>();
+    Geocoder geocoder;
+    LocationManager locationManager;
+    LocationListener locationListener;
 
 
     @Override
@@ -32,9 +49,73 @@ public class MainActivity extends Activity {
         accuracy = findViewById(R.id.accuracy_ID);
         altitude = findViewById(R.id.altitude_ID);
         address = findViewById(R.id.address_ID);
+        button = findViewById(R.id.button_ID);
+
+        geocoder = new Geocoder(this, Locale.GERMANY);
+        locationManager = (LocationManager) getApplicationContext().getSystemService(getApplicationContext().LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                Log.i("Location", location.toString());
+                try {
+                    List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(),1);
+                    Log.i("Location", addresses.get(0).toString());
+
+                    latitude.setText("Latitude: " + addresses.get(0).getLatitude());
+                    longitude.setText("Longitude: " + addresses.get(0).getLongitude());
+                    accuracy.setText("Accuracy: " + location.getAccuracy());
+                    altitude.setText("Altitude: " + location.getAltitude());
+                    address.setText("Address: \n" + addresses.get(0).getAddressLine(0));
 
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        };
+
+
+        //If we click this Button, a feature of the app gets activated which needs permission for COARSE- and FINE_LOCATION to work. So we have to check for permission before we are able to proceed witch feature Logic.
+        //If permission was previously denied, we explain why the App needs the permission and ask again.
+        button.setOnClickListener(view -> {
+            Log.i("MYInfo", "Hallo welt");
+
+            //TODO: implement the Permission Requests for Coarse and Finde Location. first attempt at lin 99-104 but its not quite working.
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+
+
+                /*
+
+
+                if(shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)){
+                    showDialogue("COARSE-LOCATION is needed", "The App needs your approximate location to work", Manifest.permission.ACCESS_COARSE_LOCATION);
+                } else requestPermissions(new String []{Manifest.permission.ACCESS_COARSE_LOCATION},0);
+                if(shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)){
+                    showDialogue("FINE-LOCATION is needed", "The App needs your approximate location to work", Manifest.permission.ACCESS_FINE_LOCATION);
+                } else requestPermissions(new String []{Manifest.permission.ACCESS_COARSE_LOCATION},0);
+
+
+                 */
+
+            } else {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+            });
+
+/*
+
+        //This is the way to do it if you want check for the permission BEFORE YOU WANT TO USE A FEATURE THAT NEEDS IT. Not useful for requesting all permissions
+        //at the start of the app
+        if (hasPermission_COARSE_LOCATION()) {
             Log.i("MyInfo","mExec1");
             // You can use the API that requires the permission.
             //TODO: performAction(...);
@@ -51,7 +132,40 @@ public class MainActivity extends Activity {
             requestPermissions(new String[] { Manifest.permission.ACCESS_COARSE_LOCATION }, 0);
         }
 
+*/
+    }
 
+
+
+    public boolean hasPermission_COARSE_LOCATION(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else return false;
+    }
+
+
+    public boolean hasPermission_FINE_LOCATION(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else return false;
+    }
+
+
+    //Asks for the Permissions that the App doesnt have yet (Without use of shouldShowRequestPermissionRationale() )
+    public void askForPermissions(){
+        permissionsToRequest.clear();
+
+        if(!hasPermission_COARSE_LOCATION()){
+            permissionsToRequest.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+
+        if(!hasPermission_FINE_LOCATION()){
+            permissionsToRequest.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+
+        if(!permissionsToRequest.isEmpty()) {
+            requestPermissions(permissionsToRequest.toArray(new String[0]), 0);
+        }
     }
 
 
@@ -73,5 +187,12 @@ public class MainActivity extends Activity {
                     }
                 })
                 .create().show();
+    }
+
+    //TODO: Implement the logic for onRequestPermissionsResult.
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
     }
 }
